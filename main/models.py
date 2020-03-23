b30403203518a39964a24f3eea3a9622c7bfc17f
Elliot Brainerd <elliotjbrainerd@gmail.com>
2020-03-23 16:45:51 -0700

    added signals to create profile when user is created

--- main/models.py
+++ main/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class ProfileManager(models.Manager):
@@ -24,14 +22,6 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-        instance.profile.followers.add(instance)
-        instance.profile.save()
 
 
 # Create your models here.

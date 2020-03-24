23f2fcecf8af3d4f44cffec3738753d6c8b04dd5
Elliot Brainerd <elliotjbrainerd@gmail.com>
2020-03-23 17:41:53 -0700

    added profile image to posts, changed profile to not include bio

--- main/forms.py
+++ main/forms.py
@@ -6,16 +6,12 @@ from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=True,
-                                 help_text='Required. Max 30 characters.')
-    last_name = forms.CharField(max_length=30, required=True,
-                                help_text='Required. Max 30 characters.')
-    bio = forms.CharField(max_length=500, required=True,
-                          help_text='Write yourself a bio here. Required. Max 500 characters.')
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required. Max 30 characters.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required. Max 30 characters.')
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'bio', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -29,12 +25,9 @@ class UserUpdateForm(forms.ModelForm):
 
 
 class ProfileUpdateForm(forms.ModelForm):
-    bio = forms.CharField(max_length=500, required=True,
-                          help_text='Enter your new bio here. Required. Max 500 characters.')
-
     class Meta:
         model = Profile
-        fields = ['bio']
+        fields = ['image']
 
 
 # class CommentForm(forms.ModelForm):
--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.views.generic import View, DetailView, ListView, UpdateView, CreateView, DeleteView
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm #UserUpdateForm, ProfileUpdateForm
 
 
 # class ProfileDetailView(DetailView):
@@ -130,7 +130,27 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'main/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated.')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'main/profile.html', context)
 
 
 # @login_required

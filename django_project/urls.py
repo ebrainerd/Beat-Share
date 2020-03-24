1075c3cf2a543fb6dd85fabde430a1d70cf5dfe1
Elliot Brainerd <elliotjbrainerd@gmail.com>
2020-03-23 23:05:53 -0700

    deleted unneeded search page template

--- django_project/urls.py
+++ django_project/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     path('post/new/', login_required(PostCreateView.as_view()), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/delete/', login_required(PostDeleteView.as_view()), name='post-delete'),
-    # path('search/', main_views.search, name="main-search"),
     path('', main_views.home, name='main-home'),
     # path('', include('main.urls')),
 ]
